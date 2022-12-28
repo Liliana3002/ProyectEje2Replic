94487e1f94be978acbb416052bec1ee1c99bcee9
Liliana <93170793+Liliana3002@users.noreply.github.com>
2022-12-28 21:17:56 +0000

    Ejercicio2

---
+++ index.js
@@ -0,0 +1,5 @@
+/*Escribe un programa que le pregunte al usuario su nombre e imprima "Hola " seguido del nombre y un signo de exclamación.
+Por ejemplo, si el usuario ingresa "Pedro" el programa debe imprimir en la consola "Hola Pedro!".*/
+
+let nombre = (prompt('¿Cuál es su nombre?'))
+console.log ('Hola' +' '+ nombre + '!')
